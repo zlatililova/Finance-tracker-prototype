cffb7e776d80f88b6dac859839dfe20726a6e7d4
Zlatina Lilova <zlati.lilova@gmail.com>
2025-12-03 15:57:03 +0200

    Add proj

---
+++ my-jsx-website/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Replace 'repo-name' with the actual name of your GitHub repository
+const repoName = 'Finance-tracker-prototype'; 
+
+export default defineConfig({
+  plugins: [react()],
+  base: `/${repoName}/`, // This is CRUCIAL for GitHub Pages
+})
